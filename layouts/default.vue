c6e6b903b7a6243b2d7cacd07cb0add4f3a9c608
Doug Lasater <dwlasater@gmail.com>
2017-12-04 01:33:20 -0600

    feat(bottom-nav): add bottom nav component to default view

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,17 @@
   <v-app light>
     <navigation></navigation>
     <nuxt/>
+    <bottom-nav></bottom-nav>
   </v-app>
 </template>
 <script>
 import navigation from '~/components/navigation'
+import bottomNav from '~/components/bottom-nav'
+
 export default{
   components: {
-    navigation
+    navigation,
+    bottomNav
   }
 }
 </script>
