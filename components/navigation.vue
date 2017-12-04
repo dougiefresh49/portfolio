a492218b4a96f69f214cf9998bd7d48582d5c328
Doug Lasater <dwlasater@gmail.com>
2017-12-04 17:40:35 -0600

    feat(navigation): add nuxt links to navigation bar

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,12 +1,12 @@
-<<template>
+<template>
     <v-toolbar light fixed dense prominent v-bind:extended="false">
-        <v-toolbar-title class="grey--text">DF</v-toolbar-title>
+        <v-toolbar-title class="grey--text"><nuxt-link to="/">DF</nuxt-link></v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-xs-only">
-            <v-btn flat>Portfolio</v-btn>
-            <v-btn flat>Open Source</v-btn>
-            <v-btn flat>Resume</v-btn>
-            <v-btn flat>About</v-btn>
+            <v-btn flat nuxt href="/portfolio">Portfolio</v-btn>
+            <v-btn flat nuxt href="/open-source">Open Source</v-btn>
+            <v-btn flat nuxt href="/resume">Resume</v-btn>
+            <v-btn flat nuxt href="/about">About</v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
