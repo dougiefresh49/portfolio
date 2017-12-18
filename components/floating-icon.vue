99f88db1a7bb0de20e1d1a9f02fa49c1098a7fa1
Doug Lasater <dwlasater@gmail.com>
2017-12-18 14:32:04 -0600

    feat(floating-icon): create floating icon componenet

---
+++ components/floating-icon.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-flex xs3>
+        <v-btn fab small :color="color" target="_blank" :href="href">
+            <img class="icon--floating" :class="padding ? padding : ''" :src="img">
+        </v-btn>
+    </v-flex>
+</template>
+<style scoped>
+    .icon--floating {
+        text-align: center;
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+    }
+</style>
+<script>
+    export default {
+      name: 'floating-icon',
+      props: {
+        color: {
+          type: String,
+          default: 'white'
+        },
+        img: String,
+        href: String,
+        padding: String
+      }
+    }
+</script>
