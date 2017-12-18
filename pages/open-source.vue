cc55c841dd9708d4eebd22abb2a61f3514954981
Doug Lasater <dwlasater@gmail.com>
2017-12-18 15:21:34 -0600

    fix(open-source): fix open source page css issues

--- pages/open-source.vue
+++ pages/open-source.vue
@@ -6,7 +6,7 @@
             <v-layout row wrap>
                 <v-flex d-flex xs12>
                     <v-card flat tile class="text-sm-center pa-5">
-                        <img src="https://ghchart.rshah.org/dougiefresh49" alt="contributions">
+                        <img class="img-fill" src="https://ghchart.rshah.org/dougiefresh49" alt="contributions">
                         <v-layout column align-center justify-center>
                             <h4 class="grey--text mb-0">GitHub Contributions</h4>
                         </v-layout>
@@ -17,7 +17,7 @@
             <!-- Repositories -->
             <v-layout row wrap>
                 <v-flex d-flex xs12 sm6>
-                    <v-card color="grey lighten-5" flat tile height="400px">
+                    <v-card color="grey lighten-5" flat tile>
                         <v-layout column align-center justify-center class="text-xs-center pa-xl">
                             <h3 class="grey--text text--darken-1">ez changelog</h3>
                             <h6 class="grey--text">A simple and easy way to generate a CHANGELOG.md based on AngularJS's changelog.js</h6>
@@ -33,23 +33,7 @@
                     </v-card>
                 </v-flex>
                 <v-flex d-flex xs12 sm6>
-                    <v-card color="blue-grey darken-3" flat tile height="400px" class="white--text">
-                        <v-layout column align-center justify-center class="text-xs-center pa-xl">
-                            <h3 class="white--text">gulp material docs</h3>
-                            <h6 class="grey--text text--lighten-3">A gulp plugin to create Material Design inspired documentation for AngularJS applications</h6>
-                            <a class="mt-2 blue--text text--darken-2"
-                               target="_blank"
-                               href="https://npm-stat.com/charts.html?package=gulp-material-docs&from=2016-01-01">
-                                <h6>2,044 Total Downloads</h6>
-                            </a>
-                            <v-btn outline color="white" class="mt-5"
-                                   target="_blank"
-                                   href="https://github.com/dougiefresh49/gulp-material-docs">View Repo</v-btn>
-                        </v-layout>
-                    </v-card>
-                </v-flex>
-                <v-flex d-flex xs12 sm6>
-                    <v-card color="blue-grey lighten-1" flat tile height="400px" class="white--text">
+                    <v-card color="blue-grey lighten-1" flat tile class="white--text">
                         <v-layout column align-center justify-center class="text-xs-center pa-xl">
                             <h3 class="white--text">project euler</h3>
                             <h6 class="grey--text text--lighten-3">A collection of my solutions in JavaScript to Project Euler's problems</h6>
@@ -65,14 +49,30 @@
                     </v-card>
                 </v-flex>
                 <v-flex d-flex xs12 sm6>
-                    <v-card color="grey lighten-5" flat tile height="400px">
+                    <v-card color="blue-grey darken-3" flat tile class="white--text">
+                        <v-layout column align-center justify-center class="text-xs-center pa-xl">
+                            <h3 class="white--text">gulp material docs</h3>
+                            <h6 class="grey--text text--lighten-3">A gulp plugin to create Material Design inspired documentation for AngularJS applications</h6>
+                            <a class="mt-2 blue--text text--darken-2"
+                               target="_blank"
+                               href="https://npm-stat.com/charts.html?package=gulp-material-docs&from=2016-01-01">
+                                <h6>2,044 Total Downloads</h6>
+                            </a>
+                            <v-btn outline color="white" class="mt-5"
+                                   target="_blank"
+                                   href="https://github.com/dougiefresh49/gulp-material-docs">View Repo</v-btn>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+                <v-flex d-flex xs12 sm6>
+                    <v-card color="grey lighten-5" flat tile>
                         <v-layout column align-center justify-center class="text-xs-center pa-xl">
                             <h3 class="grey--text text--darken-1">chrome pull request template</h3>
                             <h6 class="grey--text">Chrome extension to pre-populate pull requests with a template on GitHub and Bitbucket.</h6>
                             <a class="mt-2 blue--text text--darken-2"
                                target="_blank"
                                href="https://chrome.google.com/webstore/detail/git-pull-request-template/dlflgkjacacpmhdpiggkdiaieddfmkia?hl=en-US">
-                                <h6>Download</h6>
+                                <h6>Download Extension</h6>
                             </a>
                             <v-btn outline color="grey darken-1" class="mt-5"
                                    target="_blank"
@@ -107,5 +107,9 @@
     border-style: solid;
     border-radius: 5px;
     font-weight: 500;
+}
+.img-fill {
+    max-width: 100%;
+    max-height: 100%;
 }
 </style>
