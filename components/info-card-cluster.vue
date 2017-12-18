2129507b5d95542aa79968b0d0e73572a9906726
Doug Lasater <dwlasater@gmail.com>
2017-12-18 14:33:32 -0600

    feat(info-card): add info carf and info card cluster componenets

---
+++ components/info-card-cluster.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-layout row wrap v-for="(card, index) in cards" :key="card.title">
+            <v-flex d-flex xs12 sm6 v-if="index % 2 === 1">
+                <v-card :img="card.img" flat tile></v-card>
+            </v-flex>
+            <info-card :title="card.title"
+                       :desc="card.desc"
+                       :subtitle="'Contributions'"
+                       :details="card.details"
+                       :icons="card.icons"
+                       :readMore="card.readMore"
+                       :btn="card.button">
+            </info-card>
+            <v-flex d-flex xs12 sm6 v-if="index % 2 === 0">
+                <v-card :img="card.img" flat tile></v-card>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+<script>
+  import FloatingIcon from '~/components/floating-icon.vue'
+  import InfoCard from '~/components/info-card.vue'
+  export default {
+    props: {
+      cards: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      FloatingIcon,
+      InfoCard
+    }
+  }
+</script>
