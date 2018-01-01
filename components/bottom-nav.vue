c22da2482f46d1609cc72d38805e4bcf34113fde
Doug Lasater <dwlasater@gmail.com>
2018-01-01 13:31:21 -0600

    feat(footer): add icons and links to footer

--- components/bottom-nav.vue
+++ components/bottom-nav.vue
@@ -1,5 +1,17 @@
 <template>
   <v-footer class="pa-3">
+    <a target="_blank" href="https://twitter.com/dougiefresh49">
+      <v-icon color="grey darken-1" class="mr-3">fa-1 fa-twitter</v-icon>
+    </a>
+    <a target="_blank" href="https://www.instagram.com/dougiefresh49_/">
+      <v-icon color="grey darken-1" class="mr-3">fa-1 fa-instagram</v-icon>
+    </a>
+    <a target="_blank" href="https://www.linkedin.com/in/douglasater/">
+      <v-icon color="grey darken-1" class="mr-3">fa-1 fa-linkedin-square</v-icon>
+    </a>
+    <a target="_blank" href="https://github.com/dougiefresh49">
+      <v-icon color="grey darken-1" class="mr-3">fa-1 fa-github</v-icon>
+    </a>
     <v-spacer></v-spacer>
     <div>© {{ new Date().getFullYear() }}</div>
   </v-footer>
