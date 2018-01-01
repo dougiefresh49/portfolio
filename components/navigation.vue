2b1dd5b09d76de4bb2d5da5d290d241b93769e73
Doug Lasater <dwlasater@gmail.com>
2018-01-01 13:30:55 -0600

    fix(navigation): fix nav icon color

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,12 +1,12 @@
 <template>
     <v-toolbar light fixed dense prominent v-bind:extended="false">
-        <v-toolbar-title class="grey--text"><nuxt-link to="/">DF</nuxt-link></v-toolbar-title>
+        <v-toolbar-title><nuxt-link class="grey--text" to="/">DF</nuxt-link></v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-xs-only">
-            <v-btn flat nuxt href="/portfolio">Portfolio</v-btn>
-            <v-btn flat nuxt href="/open-source">Open Source</v-btn>
-            <v-btn flat nuxt href="/resume">Resume</v-btn>
-            <v-btn flat nuxt href="/about">About</v-btn>
+            <v-btn flat :ripple="false" nuxt href="/portfolio">Portfolio</v-btn>
+            <v-btn flat :ripple="false" nuxt href="/open-source">Open Source</v-btn>
+            <v-btn flat :ripple="false" nuxt href="/resume">Resume</v-btn>
+            <v-btn flat :ripple="false" nuxt href="/about">About</v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
