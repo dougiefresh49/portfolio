a76251ed87ef166a2c3e0d4fdf22111f858a359e
Doug Lasater <dwlasater@gmail.com>
2018-01-12 14:54:58 -0600

    feat(about): add initial about page work

---
+++ pages/about/about-data.js
@@ -0,0 +1,77 @@
+const aboutTAMU = {
+  banner: {
+    img: '/texas-am-stadium.jpg',
+    text: 'Gig \'em Aggies!'
+  },
+  title: 'Texas A&M University',
+  desc: 'Fightin\' Texas Aggie class of 2014, whoop!',
+  details: [
+    {
+      subtitle: 'Accomplishments',
+      spacing: 'single',
+      info: [
+        'Major: Computer Science',
+        'Minor: Business and Math',
+        'Major Specific GPA: ~3.6',
+        'Overall GPA: 3.2'
+      ]
+    },
+    {
+      subtitle: 'Clubs and Activities',
+      spacing: 'single',
+      info: [
+        'MSC Town Hall',
+        'Tennis',
+        'Badminton',
+        'Rock Climbing'
+      ]
+    }
+  ],
+  images: [
+    '/aggie-game-with-chey.jpg',
+    '/family-of-aggies.JPG'
+  ]
+}
+
+const aboutWork = {
+  banner: {
+    img: '/fidelity-yard.png',
+    text: ''
+  },
+  title: 'Fidelity Investments',
+  desc: 'Online Trading software engineer since 2014',
+  details: [
+    {
+      subtitle: 'Accomplishments',
+      spacing: 'single',
+      info: [
+        'Tech lead for 2 high priority projects',
+        'Helped bring in community standards for things like code and commit style',
+        'Promoted within my first 12 months',
+        ''
+      ]
+    },
+    {
+      subtitle: 'Awards',
+      spacing: 'single',
+      info: [
+        'Excellence in Action August 2017 -- Own the Outcome',
+        'Excellence in Action August 2016 -- Own the Outcome',
+        'Excellence in Action July 2015 -- Teamwork and Collaboration'
+      ]
+    }
+  ],
+  readMore: '/resume',
+  images: [
+    '/fidelity-friends.jpg',
+    '/fidelity-team-lunch.jpeg'
+  ]
+}
+
+export const AboutMeCards = [
+  aboutTAMU,
+  aboutWork
+]
+export const AboutFamilyCards = []
+export const AboutDogsCards = []
+export const AboutFunCards = []
---
+++ pages/about/index.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+        <v-parallax class="mt-5" src="/loki-doorway-copy.jpg"></v-parallax>
+        <v-container fluid grid-list-xl class="bkg-white">
+            <!-- Icons -->
+            <v-layout row>
+                <v-flex d-flex xs5 sm3>
+                    <v-card flat tile
+                            class="text-sm-center icon-card grey--text">
+                        <v-layout column align-center>
+                            <div class="icon-card-body">
+                                <v-card-media src="/doug-snow.JPG"
+                                              class="icon-img-circle"
+                                              :class="{'small': $vuetify.breakpoint.xsOnly}"
+                                              :height="($vuetify.breakpoint.xsOnly) ? '60px' : '120px'">
+                                </v-card-media>
+                                <h6 class="grey--text mt-3">Me</h6>
+                            </div>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+                <v-flex d-flex xs5 sm3>
+                    <v-card flat tile
+                            class="text-sm-center icon-card grey--text">
+                        <v-layout column align-center>
+                            <div class="icon-card-body">
+                                <v-card-media src="/doug-chey-river-sm.jpg"
+                                              class="icon-img-circle"
+                                              :class="{'small': $vuetify.breakpoint.xsOnly}"
+                                              :height="($vuetify.breakpoint.xsOnly) ? '60px' : '120px'">
+                                </v-card-media>
+                                <h6 class="grey--text mt-3">Family</h6>
+                            </div>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+                <v-flex d-flex xs5 sm3>
+                    <v-card flat tile
+                            class="text-sm-center icon-card grey--text">
+                        <v-layout column align-center>
+                            <div class="icon-card-body">
+                                <v-card-media src="/all-dogs.png"
+                                              class="icon-img-circle"
+                                              :class="{'small': $vuetify.breakpoint.xsOnly}"
+                                              :height="($vuetify.breakpoint.xsOnly) ? '60px' : '120px'">
+                                </v-card-media>
+                                <h6 class="grey--text mt-3">Dogs</h6>
+                            </div>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+                <v-flex d-flex xs5 sm3>
+                    <v-card flat tile
+                            class="text-sm-center icon-card grey--text">
+                        <v-layout column align-center>
+                            <div class="icon-card-body">
+                                <v-card-media src="/star-wars-controller.jpg"
+                                              class="icon-img-circle"
+                                              :class="{'small': $vuetify.breakpoint.xsOnly}"
+                                              :height="($vuetify.breakpoint.xsOnly) ? '60px' : '120px'">
+                                </v-card-media>
+                                <h6 class="grey--text mt-3">Fun</h6>
+                            </div>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <info-card-cluster :cards="cardsInView"></info-card-cluster>
+    </div>
+</template>
+<style scoped>
+    .icon-img {
+        width: 120px;
+        height: 120px;
+    }
+    .icon-img-circle {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+    }
+    /* todo: make small size work */
+    .icon-img.small,
+    .icon-img-circle.small {
+        width: 60px;
+        height: 60px;
+    }
+    .icon-card-body:hover {
+        cursor: pointer;
+    }
+    .icon-card {
+        text-align: center;
+    }
+    .icon-card.active {
+        border-bottom: 1px solid;
+    }
+    .section-banner--text {
+        font-weight: 600 !important;
+    }
+</style>
+<script>
+  import CardBanner from '~/components/card-banner.vue'
+  import InfoCard from '~/components/info-card.vue'
+  import InfoCardCluster from '~/components/info-card-cluster.vue'
+  import {AboutMeCards, AboutFamilyCards, AboutDogsCards, AboutFunCards} from './about-data'
+  export default {
+    components: {
+      CardBanner,
+      InfoCard,
+      InfoCardCluster
+    },
+    data () {
+      let cardsInView = AboutMeCards
+      return {
+        AboutMeCards,
+        AboutFamilyCards,
+        AboutDogsCards,
+        AboutFunCards,
+        cardsInView
+      }
+    }
+  }
+</script>
