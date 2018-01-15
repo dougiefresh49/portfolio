2ab8312db5b5bce1dd294183bafd6f42cf291a02
Doug Lasater <dwlasater@gmail.com>
2018-01-14 21:04:34 -0600

    feat(card-column): add new component for columns of images

---
+++ components/card-column.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-layout column v-if="cards">
+        <v-flex d-flex v-for="img in cards" :key="img">
+            <v-card :img="img" flat tile :height="getImageHeight(img)"></v-card>
+        </v-flex>
+    </v-layout>
+</template>
+<style scoped>
+</style>
+<script>
+    export default {
+      name: 'card-column',
+      props: {
+        cards: {
+          type: Array,
+          default: []
+        }
+      },
+      methods: {
+        getImageHeight (cardImg) {
+          if (!cardImg) {
+            return 'auto'
+          }
+          switch (this.$vuetify.breakpoint.name) {
+            case 'xs':
+              return '500px'
+            case 'sm':
+            case 'md':
+            case 'lg':
+            case 'xl':
+              return 'auto'
+          }
+        }
+      }
+    }
+</script>
