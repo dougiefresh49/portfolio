cf0c1422c77fb49df07fa1defc7f0edbdb4aeba9
Doug Lasater <dwlasater@gmail.com>
2018-01-16 02:01:52 -0600

    feat(resume): add resume page

---
+++ pages/resume.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-parallax src="/starbucks-and-laptop.JPG"></v-parallax>
+        <v-container fluid grid-list-xl class="bkg-white">
+            <v-layout column align-center justify-center class="mb-5">
+                <card-banner title="this page is still in the works"
+                             desc="Click the link to view my resume in raw PDF form"
+                             color="white"
+                             height="200">
+                </card-banner>
+                <v-btn outline color="grey darken-1" class="mb-5"
+                       target="_blank"
+                       href="/odin-onesie.JPG">View Resume</v-btn>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+<style scoped>
+</style>
+<script>
+  import CardBanner from '~/components/card-banner.vue'
+  export default {
+    components: {
+      CardBanner
+    }
+  }
+</script>
